d4a8d61ae741c3e32b94c53566fceda3d50f15c7
drashtisanghani <101659336+drashtisanghani@users.noreply.github.com>
2022-08-30 09:41:51 +0530

    hy

--- lib/view/beer_api_.dart
+++ lib/view/beer_api_.dart
@@ -24,89 +24,92 @@ class _BeerApiState extends State<BeerApi> {
       ),
 
       //-------------------------------------beer api ListView---------------------------------------------------------------------------//
-      body: ListView.builder(
-        itemCount: storeController.beerList.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
-            child: Obx(
-              //-----------------------------------------Card for View -----------------------------------------------------------------//
-              () => Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          //--------------------------------------network Image-----------------------------------------------//
-                          Image.network(
-                            storeController.beerList[index].imageUrl,
-                            height: 80,
-                            width: 50,
-                          ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    const Text("id :"),
-                                    Text(
-                                      storeController.beerList[index].id
-                                          .toString(),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    const Text("Name :"),
-                                    Text(
-                                      storeController.beerList[index].name
-                                          .toString(),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text("Description :"),
-                                    Expanded(
-                                      child: Text(
-                                        storeController
-                                            .beerList[index].description
-                                            .toString(),
-                                        style: const TextStyle(
-                                            overflow: TextOverflow.ellipsis),
-                                        maxLines: 3,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    const Text("firstBrewed :"),
-                                    Text(
+      body: buildListview(),
+      );
+  }
+  //---------------------------------------------BuildListViewBuilder---------------------------------------------------------------------//
+  Widget buildListview() {
+    return ListView.builder(
+      itemCount: storeController.beerList.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
+          child: Obx(
+            //-----------------------------------------Card for View -----------------------------------------------------------------------//
+            () => Card(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //--------------------------------------network Image------------------------------------------------//
+                        Image.network(
+                          storeController.beerList[index].imageUrl,
+                          height: 80,
+                          width: 50,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  const Text("id :"),
+                                  Text(
+                                    storeController.beerList[index].id
+                                        .toString(),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Text("Name :"),
+                                  Text(
+                                    storeController.beerList[index].name
+                                        .toString(),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text("Description :"),
+                                  Expanded(
+                                    child: Text(
                                       storeController
-                                          .beerList[index].firstBrewed
+                                          .beerList[index].description
                                           .toString(),
+                                      style: const TextStyle(
+                                          overflow: TextOverflow.ellipsis),
+                                      maxLines: 3,
                                     ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Text("firstBrewed :"),
+                                  Text(
+                                    storeController.beerList[index].firstBrewed
+                                        .toString(),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
